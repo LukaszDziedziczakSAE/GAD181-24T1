5682c424752c07d201be4bd87f33585123f72f92
Lukasz <1032314@student.sae.edu.au>
2024-02-05 11:47:14 +1030

    Initial Project Creation

---
+++ Source/GAD181_24T1/GAD181_24T1.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GAD181_24T1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GAD181_24T1, "GAD181_24T1" );
